68e7f42264f6247de8ace5123c3e0b2af08c66e9
emakafuimensah355-design <emakafuimensah355@gmail.com>
2026-07-23 12:06:14 +0400

    adding all changes

--- src/atm_prompt.js
+++ src/atm_prompt.js
@@ -15,18 +15,27 @@ let enterPin = prompt("Welcome to student Bank ATM, Please enter your 4-digit pi
 
 // 3. VALIDATE / CHECK THE PIN NUMBER
 if(enteredPin === userPin){
-    let userAmount = prompt(`PIN ACCEPTED! Your current Balance is ${userBalance}. Enter amount is to withdraw`);
-    console.log(typeof(userAmount)); //string 
-    userAmount = Number(userAmount);
-    //console.log(typeof(userAmount)); //Number
-    if(userBalance >= userAmount){
-        let newUserBalance = userBalance - userAmount;
+    let withdrawalAmount = prompt(`PIN ACCEPTED! Your current Balance is ${userBalance}. Enter amount you wish to withdraw:`);
+    //console.log(typeof(withdrawalAmount)); //string 
+    withdrawalAmount = Number(withdrawalAmount); //reassigning the withdrawalAmount variables
+    //console.log(typeof(withdrawalAmount)); //Number
+    if(withdrawalAmount >= userBalance){
+
+        let newUserBalance = userBalance - withdrawalAmount;
        // userBalance = userBalance - userAmount;
        
         receiptText.innerText = "TRANSACTION SUCCESSFUL";
+        receiptText.innerText = `Dispensing cash ... You withdrew ${userAmount}.
+        Your remaining balance is now ${userBalance - userAmount}`;  //${newUserBalance}
+     } else{
+        receiptText.innerText = "INSUFFICIENT FUNDS"
+        receiptText.innerText =`Transaction decline ... You cannot withdraw ${userAmount} because your account Balance is ${userBalance}`
      }
 } else{
-    prompt(`Enter the correct PIN`);
+    //prompt(`Enter the correct PIN`);
+    receiptTitle.innerText = "ACCESS DENIED";
+    receiptText.innerText =  "Invalid security PIN code entered. Account blocked for your protection";
+
 }
 
     
